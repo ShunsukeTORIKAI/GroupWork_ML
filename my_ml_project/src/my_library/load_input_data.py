a98ead2dd5e77e3c65f5f02707d5b0274ce370ef
ShunsukeTORIKAI <torikai-shunsuke972@g.ecc.u-tokyo.ac.jp>
2024-07-02 15:55:25 +0900

    load_input_data.pyにload_raw_data()を追加する。

--- my_ml_project/src/my_library/load_input_data.py
+++ my_ml_project/src/my_library/load_input_data.py
@@ -12,3 +12,17 @@ def load(input_path):
             else:
                 pass
     return sentence_arrays
+
+def load_raw_data(input_path):  #data.txtの一文を要素に持つリストを返す
+    sentence_arrays = []
+    with open(input_path) as f:
+        for line in f:
+            if line != "":
+                line = line.rstrip("\n")
+                sentence_arrays.append(line)
+            else:
+                pass
+    return sentence_arrays
+
+arrays = load_raw_data("data/data.txt")
+print(arrays[:10])
